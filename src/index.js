d8246dcbbff416c1a31316a2d787b1d136238cf6
Humphrey-jnr <bennyangiye202@gmail.com>
2022-07-17 19:00:35 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Header =()=>{
+  return(
+    <header>
+      <nav>
+        <img src="./logo192.png"alt="logo"></img>
+      </nav>
+    </header>
+  )
+}
 const Page =()=> {
   return (
    <div>
-      <img src="./logo192.png" width="50px" height="50px" alt="logo"></img>
+    <Header/>
       <h1>Fun Facts About React</h1>
-      <ul>
+      <ol>
         <li>Was First Released in 2013</li>
         <li>Was Originally Created by Jordan Walke</li>
         <li>Has Well Over 100k stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      </ol>
+      <Footer/>
 
    </div>
   )
 }
 
+const Footer=()=>{
+  <h4>2022 Ben Hamphrey All Rights Reserved</h4>
+}
 ReactDOM.render(<Page/>,document.getElementById("root"))
 
