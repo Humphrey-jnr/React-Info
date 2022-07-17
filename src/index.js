ee2ce45d4d4614ef50f5977e52817000f2b07fd6
Humphrey-jnr <bennyangiye202@gmail.com>
2022-07-17 20:30:35 +0300

    components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-const Header =()=>{
-  return(
-    <header>
-      <nav className="nav-bar">
-        <img src="./logo192.png"alt="logo" className="logo"></img>
-        <ul className="nav-menu">
-          <li>Pricing</li>
-          <li>Menu</li>
-          <li>About</li>
-
-        </ul>
-      </nav>
-    </header>
-  )
-}
+import Header from "./header";
 
 const Body=()=>{
   return(
